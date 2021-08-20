c04ab4cf43ee313a03b483ce8183fcebf7e49cd2
Noah Schönthal <noah.schoenthal@isolutions.ch>
2021-08-20 14:07:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace createBranchName
+{
+    static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Console.WriteLine("Type the story name in:\n");
+            string inputReadLine = Console.ReadLine();
+
+            if (inputReadLine != null)
+            {
+                inputReadLine = inputReadLine.Replace(" ", "-");
+                inputReadLine.ToLower();
+                Clipboard.SetText(inputReadLine);
+            }
+            else
+            {
+                Console.WriteLine("Please type text with space.");
+            }
+            Console.Write("Name got successfully copied.");
+            Console.Read();
+        }
+    }
+}
