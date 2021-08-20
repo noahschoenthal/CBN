9ee3ebb52e8a7062a2393bc35749c19d24df374e
Noah Schönthal <noah.schoenthal@isolutions.ch>
2021-08-20 14:30:07 +0200

    add multiple text trim options, question to abort automatical close of console

--- Program.cs
+++ Program.cs
@@ -8,21 +8,34 @@ namespace createBranchName
         [STAThread]
         private static void Main()
         {
-            Console.WriteLine("Type the story name in:\n");
-            string inputReadLine = Console.ReadLine();
+            bool endProgram = false;
+            while (!endProgram)
+            {
+                Console.WriteLine("Type the story name in:\n");
+                string inputReadLine = Console.ReadLine();
+                Console.WriteLine(
+                    "Which symbol should replace the spaces? Write one of them (to replace with blank leave it empty by pressing enter):\n '+' '-' ':' '_' '/'\n");
+                string trimCharacter = Console.ReadLine();
 
-            if (inputReadLine != null)
-            {
-                inputReadLine = inputReadLine.Replace(" ", "-");
-                inputReadLine.ToLower();
-                Clipboard.SetText(inputReadLine);
+                if (inputReadLine != null)
+                {
+                    inputReadLine = inputReadLine.Replace(" ", trimCharacter);
+                    inputReadLine.ToLower();
+                    Clipboard.SetText(inputReadLine);
+                }
+                else
+                {
+                    Console.WriteLine("Please type text with space.\n");
+                }
+
+                Console.Write("Branch name got successfully copied.\n");
+
+                Console.Write("Press 'e' to close the app, press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "e") endProgram = true;
+
+                Console.WriteLine("\n");
             }
-            else
-            {
-                Console.WriteLine("Please type text with space.");
-            }
-            Console.Write("Name got successfully copied.");
-            Console.Read();
+            return;
         }
     }
 }
